fd7280dbe0eb2fcd9632198ac8669fac91d92173
sadiqaliyev01 <aliyev.sdg@gmail.com>
2024-03-29 00:28:09 +0400

    .

--- lib/data/model/details_images_data.dart
+++ lib/data/model/details_images_data.dart
@@ -1,49 +1,48 @@
 class DetailsImagesData {
-  String hoodiePath;
-  String tshirtPath;
-  String jacketPath;
-  String pantsPath;
+  final List<String> imagePath;
 
   DetailsImagesData(
-      {required this.hoodiePath,
-      required this.jacketPath,
-      required this.pantsPath,
-      required this.tshirtPath});
+      {required this.imagePath});
 
   static List<DetailsImagesData> detailsData = [
     DetailsImagesData(
-      hoodiePath: "assets/png_images/hoodie_1.png",
-      jacketPath: "assets/png_images/jacket_1.png",
-      pantsPath: "assets/png_images/pants_1.png",
-      tshirtPath: "assets/png_images/tshirt_1.png",
+        imagePath: [
+          "assets/png_images/hoodie_1.png",
+          "assets/png_images/hoodie_2.png",
+          "assets/png_images/hoodie_3.png",
+          "assets/png_images/hoodie_4.png",
+          "assets/png_images/hoodie_5.png",
+        ],
     ),
 
     DetailsImagesData(
-      hoodiePath: "assets/png_images/hoodie_2.png",
-      jacketPath: "assets/png_images/jacket_2.png",
-      pantsPath: "assets/png_images/pants_2.png",
-      tshirtPath: "assets/png_images/tshirt_2.png",
+      imagePath: [
+        "assets/png_images/tshirt_1.png",
+        "assets/png_images/tshirt_2.png",
+        "assets/png_images/tshirt_3.png",
+        "assets/png_images/tshirt_4.png",
+        "assets/png_images/tshirt_5.png",
+      ],
     ),
 
     DetailsImagesData(
-      hoodiePath: "assets/png_images/hoodie_3.png",
-      jacketPath: "assets/png_images/jacket_3.png",
-      pantsPath: "assets/png_images/pants_3.png",
-      tshirtPath: "assets/png_images/tshirt_3.png",
+      imagePath: [
+        "assets/png_images/pants_1.png",
+        "assets/png_images/pants_2.png",
+        "assets/png_images/pants_3.png",
+        "assets/png_images/pants_4.png",
+        "assets/png_images/pants_5.png",
+      ],
     ),
 
     DetailsImagesData(
-      hoodiePath: "assets/png_images/hoodie_4.png",
-      jacketPath: "assets/png_images/jacket_4.png",
-      pantsPath: "assets/png_images/pants_4.png",
-      tshirtPath: "assets/png_images/tshirt_4.png",
-    ),
-
-    DetailsImagesData(
-      hoodiePath: "assets/png_images/hoodie_5.png",
-      jacketPath: "assets/png_images/jacket_5.png",
-      pantsPath: "assets/png_images/pants_5.png",
-      tshirtPath: "assets/png_images/tshirt_5.png",
+      imagePath: [
+        "assets/png_images/jacket_1.png",
+        "assets/png_images/jacket_2.png",
+        "assets/png_images/jacket_3.png",
+        "assets/png_images/jacket_4.png",
+        "assets/png_images/jacket_5.png",
+      ],
     ),
   ];
 }
--- lib/details_page/details_page.dart
+++ lib/details_page/details_page.dart
@@ -12,12 +12,11 @@ class DetailsScreen extends StatefulWidget {
 
 class _DetailsScreenState extends State<DetailsScreen> {
   String imageLocation = "assets/png_images/tshirt_2.png";
+  int selectedImg = 0;
 
   @override
   Widget build(BuildContext context) {
     List<DetailsImagesData> detailsData = DetailsImagesData.detailsData;
-    int index = 0;
-    final data = detailsData[index];
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
@@ -34,51 +33,51 @@ class _DetailsScreenState extends State<DetailsScreen> {
             SizedBox(width: 10),
           ],
         ),
-        body: Column(children: [
-          const SizedBox(height: 20),
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.all(10),
+        body: Column(
+          children: [
+            Center(
               child: SizedBox(
-                height: 300,
-                width: MediaQuery.of(context).size.width,
-                child: Image.asset(
-                  "",
-                  fit: BoxFit.fill,
-                ),
-              ),
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              GestureDetector(
-                onTap: () {
-                  data.hoodiePath;
-                  setState(() {});
-                },
-                child: SizedBox(
-                  height: 100,
-                  width: 100,
-                  child: Image.asset(data.hoodiePath),
-                ),
-              ),
-              GestureDetector(
-                onTap: () {
-                  data.hoodiePath;
-                  setState(() {});
-                },
-                child: SizedBox(
-                  height: 100,
-                  width: 100,
-                  child: Image.network(
-                    data.hoodiePath,
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: AspectRatio(
+                  aspectRatio: 1,
+                  child: Container(
+                    decoration: BoxDecoration(color: AppColors.iconsColor),
+                    child: Image.asset(
+                      detailsData[selectedImg].imagePath[selectedImg],
+                    ),
                   ),
                 ),
               ),
-            ],
-          ),
-        ]),
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ...List.generate(
+                  detailsData.length,
+                  (index) => GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectedImg = index;
+                      });
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      height: 100,
+                      width: 100,
+                      decoration: BoxDecoration(
+                        color: AppColors.iconsColor,
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Image.asset(
+                          detailsData[selectedImg].imagePath[index]),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
